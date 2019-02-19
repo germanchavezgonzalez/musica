08a02eb2c9b90bb37efd99f5d15fba7b35df2f1a
germanchavezgonzalez <47173820+germanchavezgonzalez@users.noreply.github.com>
2019-02-19 13:51:08 +0100

    Update conectar.php

--- db/conectar.php
+++ db/conectar.php
@@ -4,7 +4,7 @@ echo "Iniciando conexion BD"."<br>";
 // Creación de una clase que nos permita conectarnos a la BD
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "", "musica");
+        $conexion=new mysqli("10.131.16.201", "root", "rootroot", "musica");
 		if($conexion!=null){
 	    return $conexion;
 		}else{
